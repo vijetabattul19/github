a8d0f8f1c61b62460dd04a78d80b0d7b9b1f88dc
prade <prade@Vijeta-PC>
2023-08-20 22:48:41 +0530

    added float

--- github/src/githubsetup/GitDemo.java
+++ github/src/githubsetup/GitDemo.java
@@ -2,8 +2,15 @@ package githubsetup;
 
 public class GitDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+	
 		System.out.println(" Welcome to GitHub!!!");
+		
+		float b = 10.10f;
+		
+		
+		
 
 	}
 
